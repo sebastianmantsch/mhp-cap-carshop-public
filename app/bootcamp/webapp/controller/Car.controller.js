8b90d8a87bdd0b361d7c54a0546b2dcbe9a65ae6
Mark Deppe <mark.deppe@mhp.com>
2023-06-27 09:46:15 +0000

    Added binding

--- app/bootcamp/webapp/controller/Car.controller.js
+++ app/bootcamp/webapp/controller/Car.controller.js
@@ -28,10 +28,31 @@ sap.ui.define([
 
         _onRouteMatched: function (oEvent) {
 
-            // Set the layout to three Column layout
-            this.getModel("appView").setProperty("/layout", "ThreeColumnsMidExpanded");
+            // Read routing arguments for later use (e.g. back navigation)
+	        this.sManufacturerID = oEvent.getParameter("arguments").objectId;
+	        this.sCarID = oEvent.getParameter("arguments").carId; 
 
-        }
+	        // Set the layout to three Column layout
+	        this.getModel("appView").setProperty("/layout", "ThreeColumnsMidExpanded"); 
+
+            // After the metamodel was loaded (Promise) create the binding path
+            this.getModel().metadataLoaded().then(() => {
+                const sCarPath = this.getModel().createKey("Cars", {
+                    ID: this.sCarID
+                }); 
+
+                // Bind this car to the view
+                this._bindView("/" + sCarPath);
+            });
+
+        },
+
+        _bindView: function (sObjectPath) {
+            this.getView().bindElement({
+                path: sObjectPath
+            });
+        },
+        
 
     });
 
